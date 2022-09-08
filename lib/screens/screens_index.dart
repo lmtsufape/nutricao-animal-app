e07bdf0171e4d12f078e79d76a23338e1b30b176
Bielmelo6 <gabrielmelodemenezes@gmail.com>
2022-09-08 20:49:51 -0300

    Mudança na rota da home screen no arquivo index

--- lib/screens/screens_index.dart
+++ lib/screens/screens_index.dart
@@ -3,7 +3,7 @@ class Screens {
   ///so you can easily navigate to them.
   ///With this, you can change the route easily without breaking the routes and you can
   ///change in the future
-  static const String home = '/home';
+  static String home = '/home';
   static const String splash = '/splash';
   static const String signin = '/sign_in';
   static const String signup = '/sign_up';
