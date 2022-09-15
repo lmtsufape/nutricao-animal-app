ddc2d53336e1a359240d0319410d356143a0c7da
Rievvy <thiagoalmeidadepaiva@gmail.com>
2022-09-15 16:18:04 -0300

    finalizado o post de usuários na API

--- lib/screens/sign up/sign_up_repository.dart
+++ lib/screens/sign up/sign_up_repository.dart
@@ -1,9 +1,16 @@
 import 'package:dio/dio.dart';
-import 'package:thunderapp/screens/sign%20in/sign_in_controller.dart';
+import 'package:flutter/material.dart';
+
 import 'package:http/http.dart' as http;
 
-class SignUpRepository extends SignInController {
-  void postUsers(name, email, password) {
-    
+class SignUpRepository {
+  static void signUp(name, email, password) async {
+    http.Response response =
+        await http.post(Uri.parse('http://127.0.0.1:8000/api/user/'), body: {
+      'name': name,
+      'email': email,
+      'password': password,
+    });
+    print(response.statusCode);
   }
 }
--- lib/screens/sign up/sign_up_screen.dart
+++ lib/screens/sign up/sign_up_screen.dart
@@ -3,11 +3,11 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
+
 import 'package:provider/provider.dart';
 import 'package:thunderapp/components/forms/custom_text_form_field.dart';
 import 'package:thunderapp/screens/sign%20up/sign_up_controller.dart';
+import 'package:thunderapp/screens/sign%20up/sign_up_repository.dart';
 import 'package:thunderapp/shared/constants/app_number_constants.dart';
 import 'package:thunderapp/shared/constants/style_constants.dart';
 
@@ -15,6 +15,7 @@ import '../../components/buttons/primary_button.dart';
 import '../../components/utils/vertical_spacer_box.dart';
 import '../../shared/constants/app_enums.dart';
 import '../screens_index.dart';
+import 'sign_up_repository.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -24,16 +25,6 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
-  final _nameController = TextEditingController();
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  final formKey = GlobalKey<FormState>();
-
-  TextEditingController get nameController => _nameController;
-  TextEditingController get emailController => _emailController;
-  TextEditingController get passwordController => _passwordController;
-
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -65,44 +56,47 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       'Nome de exibição',
                       style: TextStyle(color: kBackgroundColor),
                     ),
-                    TextFormField(
-                      //hintText: 'Ana Vieira',
-                      controller: nameController,
+                    CustomTextFormField(
+                      hintText: 'Ana Vieira',
+                      controller: controller.nameController,
                     ),
                     const VerticalSpacerBox(size: SpacerSize.small),
                     const Text(
                       'E-mail',
                       style: TextStyle(color: kBackgroundColor),
                     ),
-                    TextFormField(
-                      //hintText: 'email@example.com',
-                      controller: emailController,
+                    CustomTextFormField(
+                      hintText: 'email@example.com',
+                      controller: controller.emailController,
                     ),
                     const VerticalSpacerBox(size: SpacerSize.small),
                     const Text('Senha',
                         style: TextStyle(color: kBackgroundColor)),
-                    TextFormField(
-                      //hintText: '********',
-                      //isPassword: true,
-                      controller: passwordController,
+                    CustomTextFormField(
+                      hintText: '********',
+                      isPassword: true,
+                      controller: controller.passwordController,
                     ),
                     const VerticalSpacerBox(size: SpacerSize.small),
                     const Text('Confirmar senha',
                         style: TextStyle(color: kBackgroundColor)),
                     const CustomTextFormField(
                       hintText: '********',
-                      //isPassword: true,
-                      // controller: controller.passwordController,
+                      isPassword: true,
                     ),
                     const VerticalSpacerBox(size: SpacerSize.small),
                     const SizedBox(
                       height: kMediumSize,
                     ),
                     Center(
-                      child: ElevatedButton(
-                          child: const Text('Finalizar'),
+                      child: PrimaryButton(
+                          text: const Text('Finalizar'),
                           onPressed: () {
-                            signUpt();
+                            SignUpRepository.signUp(
+                              controller.nameController.text,
+                              controller.emailController.text,
+                              controller.passwordController.text,
+                            );
                           }),
                     ),
                     const Spacer(),
@@ -112,22 +106,5 @@ class _SignUpScreenState extends State<SignUpScreen> {
         );
       },
     );
-  }
-
-  void signUp(name, email, password) async {
-    Response response =
-        await http.post(Uri.parse('http://127.0.0.1:8000/api/user/'), body: {
-      'name': nameController.text,
-      'email': emailController.text,
-      'password': passwordController.text
-    });
-    print(response.statusCode);
-  }
-
-  void signUpt() async {
-    Response response = await http.get(
-      Uri.parse('http://127.0.0.1:8000/api/user/'),
-    );
-    print(response.statusCode);
   }
 }
