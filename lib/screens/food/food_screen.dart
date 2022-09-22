0fed292adce4c7c873017fd576b9113f7810c3e4
LMTS <gabrielmelodemenezes@gmail.com>
2022-09-22 17:18:57 -0300

    Adicionando botão na food_screen

--- lib/screens/food/food_screen.dart
+++ lib/screens/food/food_screen.dart
@@ -8,6 +8,8 @@ import 'package:thunderapp/shared/constants/app_number_constants.dart';
 import 'package:thunderapp/shared/constants/app_theme.dart';
 import 'package:thunderapp/shared/constants/style_constants.dart';
 
+enum PrivateMenu {yes, no}
+
 class FoodScreen extends StatelessWidget {
   const FoodScreen({Key? key}) : super(key: key);
 
@@ -22,8 +24,8 @@ class FoodScreen extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 18, top: 16),
+          const Padding(
+            padding: EdgeInsets.only(left: 18, top: 20),
             child: Text(
               'Alimentar',
               style: TextStyle(
@@ -32,8 +34,8 @@ class FoodScreen extends StatelessWidget {
                   fontWeight: FontWeight.w900),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(
+          const Padding(
+            padding: EdgeInsets.only(
               left: 19,
               bottom: 16,
             ),
@@ -42,8 +44,8 @@ class FoodScreen extends StatelessWidget {
                     TextStyle(color: kSecondaryColor, fontSize: kMediumSize)),
           ),
           TextFieldButtonPC('Escolher do cardápio'),
-          Padding(
-            padding: const EdgeInsets.only(top: 16, bottom: 16),
+          const Padding(
+            padding: EdgeInsets.only(top: 16, bottom: 16),
             child: Center(
                 child: Text(
               '-------------------- OU --------------------',
@@ -53,8 +55,8 @@ class FoodScreen extends StatelessWidget {
                   fontWeight: FontWeight.w900),
             )),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 22, bottom: 8),
+          const Padding(
+            padding: EdgeInsets.only(left: 22, bottom: 8),
             child: Text(
               'Comida fora do cardápio',
               style: TextStyle(
@@ -66,13 +68,18 @@ class FoodScreen extends StatelessWidget {
           TextFieldButton('Tipo'),
           TextFieldCustom('Comida'),
           TextFieldCustom('Quantidade(em gramas)'),
+          const MenuWidget(),
           Center(
             child: Padding(
               padding: const EdgeInsets.only(top: 16),
-              child: ElevatedButton(
-                style: styleAlimentar,
-                onPressed: () {},
-                child: Text('Alimentar', style: TextStyle(color: kBackgroundColor)),
+              child: SizedBox(
+                width: 120,
+                height: 40,
+                child: ElevatedButton(
+                  style: styleAlimentar,
+                  onPressed: () {},
+                  child: const Text('Alimentar', style: TextStyle(color: kBackgroundColor, fontSize: kMediumSize)),
+                ),
               ),
             ),
           ),
@@ -81,6 +88,49 @@ class FoodScreen extends StatelessWidget {
     );
   }
 }
+
+class MenuWidget extends StatefulWidget {
+  const MenuWidget({super.key});
+
+  @override
+  State<MenuWidget> createState() => _MenuWidgetState();
+}
+
+class _MenuWidgetState extends State<MenuWidget> {
+  PrivateMenu? _privateMenu;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+          title: const Text(
+      'Adicionar no cárdapio particular?',
+      style: TextStyle(
+          color: kPrimaryColor,
+          fontWeight: FontWeight.w900,
+          fontSize: 20),
+    ),
+          leading: Transform.scale(
+            scale: 2,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 2),
+              child: Radio<PrivateMenu>(
+              toggleable: true,
+              activeColor: kDetailColor,
+              value: PrivateMenu.yes,
+              groupValue: _privateMenu,
+              onChanged: (PrivateMenu? value) {
+                setState(() {
+                  _privateMenu = value;
+                });
+              },
+              ),
+            ),
+          ),
+        );
+  }
+}
+
+
 
 class TextFieldButtonPC extends StatelessWidget {
   final String _buttonPCFieldLabel;
@@ -96,41 +146,39 @@ class TextFieldButtonPC extends StatelessWidget {
     return InkWell(
       onTap: () {},
       child: Ink(
-        child: Container(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  _buttonPCFieldLabel,
-                  style: TextStyle(
-                    color: kPrimaryColor,
-                    fontSize: kMediumLargeSize,
-                    fontWeight: FontWeight.w900,
-                  ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20, bottom: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                _buttonPCFieldLabel,
+                style: const TextStyle(
+                  color: kPrimaryColor,
+                  fontSize: kMediumLargeSize,
+                  fontWeight: FontWeight.w900,
                 ),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Selecione',
-                    hintStyle: TextStyle(fontSize: 18),
-                    suffixIcon: Padding(
-                      padding: const EdgeInsets.only(right: 10.0),
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.arrow_circle_down_sharp,
-                          size: 35,
-                          color: kDetailColor,
-                        ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Selecione',
+                  hintStyle: const TextStyle(fontSize: 18),
+                  suffixIcon: Padding(
+                    padding: const EdgeInsets.only(right: 10.0),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.arrow_circle_down_sharp,
+                        size: 35,
+                        color: kDetailColor,
                       ),
                     ),
-                    contentPadding: EdgeInsets.all(14),
-                    border: OutlineInputBorder(),
                   ),
+                  contentPadding: const EdgeInsets.all(14),
+                  border: const OutlineInputBorder(),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
