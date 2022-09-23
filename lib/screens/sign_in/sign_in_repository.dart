b6e1b3c66241676e5b0d6d6266164c1ab4ca18fa
Rievvy <thiagoalmeidadepaiva@gmail.com>
2022-09-23 14:45:56 -0300

    Inicio da autenticação de login

---
+++ lib/screens/sign_in/sign_in_repository.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:thunderapp/shared/constants/app_text_constants.dart';
+import 'package:thunderapp/shared/core/models/user_model.dart';
+import 'package:dio/dio.dart' as dio;
+
+class SignInRepository {
+  final _dio = Dio();
+
+  Future<bool> login(String email, String password) async {
+    final UserModel userModel = UserModel();
+
+    var response = await _dio.post('$kBaseUrl/login',
+        options: Options(
+          headers: {
+            "Content-Type": "application/json",
+            "Accepted": "application/json"
+          },
+        ),
+        data: {
+          "email": email,
+          "password": password,
+        });
+
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
