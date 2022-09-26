7b1ad2cfe3c46968e55ea3a65664d1233624a6a6
Rievvy <thiagoalmeidadepaiva@gmail.com>
2022-09-26 16:13:56 -0300

    Mudança na AppBar

--- lib/screens/user screen/user_screen.dart
+++ lib/screens/user screen/user_screen.dart
@@ -9,7 +9,7 @@ class UserScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarCustom(context),
+      appBar: AppBar(),
     );
   }
 }
