0fe8ae207a5ec2ea59a5b8e8f3c3b176344216f8
Caio <caio.vsg@hotmail.com>
2023-01-10 10:48:57 -0300

    Adicionada funcionalidade de LogOff do User

--- lib/screens/user screen/user_screen.dart
+++ lib/screens/user screen/user_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:thunderapp/components/buttons/primary_button.dart';
+import 'package:thunderapp/screens/user%20screen/user_screen_controller.dart';
 import 'package:thunderapp/shared/constants/app_theme.dart';
 
 class UserScreen extends StatelessWidget {
@@ -8,8 +10,14 @@ class UserScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final UserScreenController controller = UserScreenController();
     return Scaffold(
       appBar: AppBar(),
+      body: Center(
+        child: PrimaryButton(
+            text: const Text("Log off"),
+            onPressed: () => controller.logoff(context)),
+      ),
     );
   }
 }
