687169e124eeea910f69d9995747ae2697109d6a
JoseDanielF <danielduartefilho.df@gmail.com>
2023-01-18 16:55:45 -0300

    Refatorando código morto

--- lib/screens/add_animal/add_animal_repository.dart
+++ lib/screens/add_animal/add_animal_repository.dart
@@ -1,12 +1,6 @@
-import 'dart:convert';
-
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:thunderapp/screens/add_animal/add_animal_screen.dart';
 import 'package:thunderapp/shared/constants/app_text_constants.dart';
 import 'package:dio/dio.dart';
-import 'package:thunderapp/shared/core/models/breed_model.dart';
 
 class AddAnimalRepository {
   late int userId;
