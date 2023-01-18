34ada9d55f36b200a3636878fa449b5c8cfb9df2
JoseDanielF <danielduartefilho.df@gmail.com>
2023-01-18 17:27:43 -0300

    Dialogs alerts adicionados

---
+++ lib/shared/components/dialogs/default_dialog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DefaultDialog extends StatelessWidget {
+  const DefaultDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
