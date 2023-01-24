6fafc1f3d277e64ee122a6996935fe299d7c7c68
JoseDanielF <danielduartefilho.df@gmail.com>
2023-01-24 18:13:55 -0300

    Corrigindo textos

--- lib/app.dart
+++ lib/app.dart
@@ -1,30 +1,21 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_wrapper.dart';
-
 import 'package:thunderapp/screens/add_animal/add_animal_screen.dart';
-import 'package:thunderapp/screens/animal_details/animal_details_screen.dart';
 import 'package:thunderapp/screens/edit_animal/edit_animal_screen.dart';
 import 'package:thunderapp/screens/edit_food/edit_food_screen.dart';
 import 'package:thunderapp/screens/edit_profile/edit_profile_screen.dart';
-
 import 'package:thunderapp/screens/animal/animal_screen.dart';
-
-import 'package:thunderapp/screens/food/food_screen.dart';
 import 'package:thunderapp/screens/food_details/food_details_screen.dart';
 import 'package:thunderapp/screens/no_reg_food/no_reg_food_screen.dart';
 import 'package:thunderapp/screens/private_menu/private_menu_screen.dart';
 import 'package:thunderapp/screens/screens_index.dart';
 import 'package:thunderapp/screens/share_tutoring/share_tutoring_screen.dart';
-
 import 'package:thunderapp/screens/sign_in/sign_in_screen.dart';
 import 'package:thunderapp/screens/sign_up/sign_up_screen.dart';
 import 'package:thunderapp/screens/user%20screen/user_screen.dart';
-
 import 'package:thunderapp/shared/constants/app_theme.dart';
-
 import 'screens/home/home_screen.dart';
-
 import 'screens/splash/splash_screen.dart';
 
 class App extends StatelessWidget {
@@ -62,7 +53,8 @@ class App extends StatelessWidget {
             const EditProfileScreen(),
         Screens.shareTutoring: (BuildContext context) =>
             const ShareTutoringScreen(),
-        Screens.privateMenu: (BuildContext context) => PrivateMenuScreen(),
+        Screens.privateMenu: (BuildContext context) =>
+            const PrivateMenuScreen(),
         Screens.editFood: (BuildContext context) => const EditFoodScreen(),
         Screens.foodDetails: (BuildContext context) =>
             const FoodDetailsScreen(),
