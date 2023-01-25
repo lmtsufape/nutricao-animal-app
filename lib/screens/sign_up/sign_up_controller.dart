ac043be97894a63531b5cda54dda85ec0d238c2f
Caio <caio.vsg@hotmail.com>
2023-01-25 17:27:47 -0300

    validate da tela de cadastro

--- lib/screens/sign_up/sign_up_controller.dart
+++ lib/screens/sign_up/sign_up_controller.dart
@@ -26,12 +26,25 @@ class SignUpController with ChangeNotifier {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
 
   //Recebe uma string
   TextEditingController get nameController => _nameController;
   TextEditingController get emailController => _emailController;
   TextEditingController get passwordController => _passwordController;
+  TextEditingController get confirmPasswordController =>
+      _confirmPasswordController;
 
   String? erroMessage;
   var status = SignUpStatus.idle;
+}
+
+bool validateFields(String nome, String email, String senha, String confSenha) {
+  if (nome.isEmpty || email.isEmpty || senha.isEmpty || confSenha.isEmpty) {
+    return false;
+  } else if (senha != confSenha) {
+    return false;
+  }
+  return true;
 }
