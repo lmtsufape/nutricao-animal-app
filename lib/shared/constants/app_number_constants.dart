8889cbfd66f315a18fd075ab4999ae171f8d5ba3
Bielmelo6 <gabrielmelodemenezes@gmail.com>
2023-03-09 00:56:25 -0300

    Mudança nas constantes de tamanho para auxiliar na responsividade

--- lib/shared/constants/app_number_constants.dart
+++ lib/shared/constants/app_number_constants.dart
@@ -2,9 +2,9 @@ const double kDefaultPadding = 32.0;
 
 const double kDefaultBorderRadius = 8.0;
 
-const double kTinySize = 8;
-const double kSmallSize = 12;
-const double kMediumSize = 16;
-const double kMediumLargeSize = 18.0;
-const double kLargeSize = 24;
-const double kHugeSize = 32;
+const double kTinySize = 0.08;
+const double kSmallSize = 0.012;
+const double kMediumSize = 0.016;
+const double kMediumLargeSize = 0.018;
+const double kLargeSize = 0.024;
+const double kHugeSize = 0.032;
