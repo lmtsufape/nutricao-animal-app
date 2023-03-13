8934e0c2abca0303e615b6732b04c48cea1dcd4e
pedronso <pedrocaetanoaraujopc@gmail.com>
2023-03-13 17:47:58 -0300

    Reformulação da HomeScreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -227,99 +227,143 @@ class _CardHomeScreenState extends State<CardHomeScreen> {
       child: Ink(
         child: Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Card(
-              color: kPrimaryColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              child: Column(mainAxisSize: MainAxisSize.max, children: [
-                ListTile(
-                  leading: const Padding(
-                    padding: EdgeInsets.only(top: 100.0, left: 80.0),
-                    child: Icon(Icons.add_photo_alternate_outlined),
-                  ),
-                  title: Column(
-                    children: [
-                      // Padding(
-                      //   padding: const EdgeInsets.only(
-                      //     top: 24,
-                      //     right: 16,
-                      //   ),
-                      //   child: Text(
-                      //     widget.age.toString(),
-                      //     style:
-                      //         TextStyle(fontSize: 15, color: kBackgroundColor),
-                      //   ),
-                      // ),
+            child: SizedBox(
+              height: heightScreen * 0.25,
+              child: Card(
+                color: kPrimaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Container(
+                  color: Colors.greenAccent,
+                  child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+
                       Padding(
-                        padding: const EdgeInsets.only(top: 17.0, left: 47.0),
-                        child: Text(
-                          widget.animal.name.toString(),
-                          style: TextStyle(
-                              fontSize: heightScreen * 0.035,
-                              color: kBackgroundColor,
-                              fontWeight: FontWeight.w900),
-                        ),
-                      ),
-                    ],
-                  ),
-                  subtitle: Padding(
-                    padding: const EdgeInsets.only(left: 85.0, bottom: 25),
-                    child: Text(
-                      //Trocar para widget.animal.breed quando arrumar API
-
-                      widget.animal.breed.toString(),
-                      style: const TextStyle(
-                        fontSize: 20.0,
-
-                        color: kBackgroundColor,
-                      ),
-                    ),
-                  ),
-                  trailing: Padding(
-                    padding: const EdgeInsets.only(right: 10.0, top: 10.0),
-                    child: Text(
-                      widget.animal.sex.toString(),
-                      style: TextStyle(
-                          color: kBackgroundColor, fontSize: heightScreen * 0.015),
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(8.0, 8.0, 20.0, 22.0),
-                      child: SizedBox(
-                        width: widthScreen * 0.4,
-                        height: heightScreen * 0.04,
-                        child: ElevatedButton.icon(
-                          style: style,
-                          //exemplo
-                          onPressed: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      FoodScreen(widget.animal.id))),
-                          icon: Icon(
-                            Icons.restaurant_menu,
-                            color: kBackgroundColor,
-                            size: widthScreen * 0.06,
-                            textDirection: TextDirection.ltr,
-                          ),
-                          label: Text(
-                            'Alimentar',
-                            style: TextStyle(
-                                color: kBackgroundColor,
-                                fontSize: heightScreen * 0.022,
-                                fontWeight: FontWeight.w700),
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Container(
+                          color: Colors.pinkAccent,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                   top: 5,
+                                   right: 16,
+                                 ),
+                                 child: Text(
+                                   //widget.age.toString(),
+                                   "5",
+                                   style:
+                                       TextStyle(fontSize: heightScreen * kMediumSize, color: kBackgroundColor),
+                                 ),
+                               ),
+                              Padding(
+                                padding: const EdgeInsets.only(right: 10.0, top: 5.0),
+                                child: Text(
+                                  widget.animal.sex.toString(),
+                                  style: TextStyle(
+                                      color: kBackgroundColor, fontSize: heightScreen * kMediumSize),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          child: SizedBox(
+                            width: widthScreen * 0.4,
+                            child: Icon(Icons.add_a_photo, size: 80, color: kDetailColor,),
+                          ),
+                        ),
+                        Container(
+                          constraints: const BoxConstraints.tightForFinite(width: double.infinity, height: double.infinity),
+                          alignment: Alignment.topRight,
+                          color: Colors.deepPurpleAccent,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 18.0),
+                                child: FittedBox(
+                                  fit: BoxFit.scaleDown,
+                                  child: Container(
+                                    color: kDetailColor,
+                                    child: Text(
+                                      widget.animal.name.toString(),
+                                      textDirection: TextDirection.ltr,
+                                      style: TextStyle(
+                                        fontSize: heightScreen * kLargeSize,
+                                        color: kBackgroundColor,
+                                        fontWeight: FontWeight.w900,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 25),
+                                child: Container(
+                                  color: Colors.red,
+                                  child: Text(
+                                    //Trocar para widget.animal.breed quando arrumar API
+                                    widget.animal.breed.toString(),
+                                    style: TextStyle(
+                                      fontSize: heightScreen * 0.017,
+                                      color: kBackgroundColor,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(8.0, 10, 14.0, 8.0),
+                          child: SizedBox(
+                            width: widthScreen * 0.45,
+                            height: heightScreen * 0.042,
+                            child: ElevatedButton.icon(
+                              style: style,
+                              //exemplo
+                              onPressed: () => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          FoodScreen(widget.animal.id))),
+                              icon: Padding(
+                                padding: const EdgeInsets.only(right: 6, left: 0),
+                                child: Icon(
+                                  Icons.add_circle_outline_rounded,
+                                  color: kBackgroundColor,
+                                  size: widthScreen * 0.08,
+                                  textDirection: TextDirection.ltr,
+                                ),
+                              ),
+                              label: Center(
+                                child: Text(
+                                  'Alimentar',
+                                  style: TextStyle(
+                                      color: kBackgroundColor,
+                                      fontSize: heightScreen * kMediumLargeSize,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ]),
                 ),
-              ]),
+              ),
             )),
       ),
     );
